9f0513c22c175a729b3d99317d3a977d5f2f1351
kenomun <eugenio.mun.f@gmail.com>
2025-05-30 12:32:02 -0400

    se crea el end poin Get para traer todos los tasks

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const tasksController = require('../controllers/tasks.controller');
 
 router.post('/', tasksController.createTask);
+router.get('/', tasksController.getAllTasks);
 
 module.exports = router;
